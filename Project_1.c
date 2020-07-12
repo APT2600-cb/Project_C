77c8c7e9e8cdd9c01f85cbcf7de7fb23cec64c3f
APT2600-cb <67943202+APT2600-cb@users.noreply.github.com>
2020-07-12 18:05:56 +0700

    Add files via upload

---
+++ Project_1.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+
+//Work in
+int main()
+{
+
+    //Data_1
+    char Name[] = "";
+    int i;
+	//Banner
+    printf("\n\n");
+
+    printf("Ahutor : APT-2600");
+
+    printf("\n\n");
+
+    printf("Name is : ");
+
+    scanf ("%s", Name);
+
+    printf("\n\n");
+
+    for (i = 0; i <= 5; i++)
+    {
+        //statements_1
+        printf("Hi %s !\n", Name);
+    }
+}
